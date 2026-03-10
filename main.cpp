a554f1bc0174577bf35b7b3e5cff934f61b82160
a <a>
2026-03-10 11:05:56 +0100

    Functionalised "Student" class

--- main.cpp
+++ main.cpp
@@ -3,26 +3,73 @@
 #include <iostream>
 #include <numeric>
 
-struct Student {
+class Student {
     std::string name;
     std::string surname;
     std::vector<float> grades;
+    int album;
+public:
+    Student(std::string n, std::string s, int a){
+        name = n; surname = s;
+        if(a<1000000&&a>=10000) album = a;
+        else std::cout << "Provided album number is invalid";
+    }
 
     float calculate_grade() {
         float sum = std::accumulate(grades.begin(), grades.end(), 0.0f);
         return sum / grades.size();
     }
-    void print() {
-        std::cout<<name<<" "<<surname<<": ";
+
+    bool add_grade(float grade) {
+        if (grade >= 2.0 && grade <= 5.0) {
+            grades.push_back(grade);
+            return true;
+        }
+    return false;
+    }
+
+    void set_name(std::string n, std::string s){
+        name = n; surname = s;
+    }
+
+    bool set_album(int a){
+        if(a<1000000&&a>=10000) {
+            album = a;
+            return true;
+        }
+        std::cout << "Provided album number is invalid";
+        return false;
+    }
+
+    void summary() {
+        std::cout<<album<<" - "<<name<<" "<<surname<<": ";
         for(int i=0;i<grades.size();i++){
             std::cout<<grades[i]<<" ";
         }
         std::cout << std::endl;
     }
+    bool passed() {
+        int fail=0;
+        for(int i=0;i<grades.size();i++){
+            if(grades[i]==2.0)fail++;
+        }
+        if(fail<=1)return true;
+        return false;
+    }
 };
 
 int main(){
-    Student student{"Some", "Student", {2, 3, 4, 5, 3}};
-    std::cout << student.calculate_grade() << std::endl;
-    student.print();
+    Student student("Some","Student",192467);
+    for(float f=2.0;f<=5.0;f+=0.5){
+        student.add_grade(f);
+    }
+    student.summary();
+    std::cout << "Mean grade: " << student.calculate_grade() << std::endl;
+    student.set_album(342216);
+    student.set_name("John","Doe");
+    student.add_grade(4.0);
+    student.summary();
+    std::cout << "Mean grade: " << student.calculate_grade() << std::endl;
+    if(student.passed())std::cout<<"This student has passed\n";
+    else std::cout<<"This student has not passed\n";
 }
